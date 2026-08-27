1925c38dfdcfcd890000063d2ed6829b9c08c14d
quinnarnold <qarnold005@gmail.com>
2026-08-27 10:34:50 -0400

    fix(rollout): separate transfer and policy versions

--- unirl/rollout/engine/sglang/engine.py
+++ unirl/rollout/engine/sglang/engine.py
@@ -160,6 +160,7 @@ class SGLangRolloutEngine(BaseRolloutEngine):
         )
 
         self._version = 0
+        self._weight_update_calls = 0
 
     def _prepare_generation(self, sample: Sample) -> Any:
         sampling = resolve_sampling(self.cfg, sample)
@@ -297,7 +298,7 @@ class SGLangRolloutEngine(BaseRolloutEngine):
             load_format=load_format,
             flush_cache=flush_cache,
         )
-        self._version += 1
+        self._weight_update_calls += 1
 
     def init_weights_update_group(
         self,
@@ -344,7 +345,7 @@ class SGLangRolloutEngine(BaseRolloutEngine):
             group_name=group_name,
             flush_cache=flush_cache,
         )
-        self._version += 1
+        self._weight_update_calls += 1
 
     def destroy_weights_update_group(
         self,
